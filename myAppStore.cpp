c6d31281480a99ed36d0f96a29f2faaaa6aaee82
joseph <josephferuglio@gmail.com>
2020-10-15 19:57:05 -0700

    finished the requirements for the milestone by implementing the hash table, bst, and query command 'find app', also completed the portion to delete the dynamically allocated arrays

--- myAppStore.cpp
+++ myAppStore.cpp
@@ -1,14 +1,22 @@
-//
-// Created by josep on 10/14/2020.
-//
+/*
+ * Name: Joseph Feruglio
+ * Class: CSE310
+ * Assignment: Project 2 Milestone
+ *
+ */
+/*
+ * This project milestone creates an app store, where it will read in data with cin
+ * storing the data in a hash table for quick look up and a binary search tree as the database
+ * currently supports one query function: 'find app'
+ */
 
-#include "myAppStore.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <string.h>
 #include <iostream>
 #include <math.h>
+#include <iomanip>
 
 using namespace std;
 
@@ -38,6 +46,11 @@ struct hash_table_entry{
     struct hash_table_entry *next;
 };
 
+/**
+ * This function will check if the number is a prime or not
+ * @param val The number to be checked if is a prime
+ * @return True or False value
+ */
 bool TestForPrime ( int val){
     int limit, factor = 2;
 
@@ -47,6 +60,20 @@ bool TestForPrime ( int val){
 
 
     return (factor > limit);
+}
+
+/**
+ * This function recursively deletes the nodes from the bst
+ * @param node The root
+ */
+void deleteNode(struct tree* node){
+    if(node->left != nullptr){
+        deleteNode(node->left);
+    }
+    if(node->right != nullptr){
+        deleteNode(node->right);
+    }
+    delete node;
 }
 
 int main(){
@@ -61,14 +88,14 @@ int main(){
     int howManyCategories = stoi(input);
 
     // dynamically allocate an array of size howManyCategories of type struct categories
-    categories *app_categories = new categories[howManyCategories];
+    auto *app_categories = new categories[howManyCategories];
 
     // loops through the array of categories setting the names of each category
     // and sets the root of the bst to NULL
     for(int i = 0; i < howManyCategories; i++){
         getline(cin, input);
         app_categories[i].category = input;
-        app_categories[i].root = NULL;
+        app_categories[i].root = nullptr;
     }
 
     // get the number of applications
@@ -90,11 +117,12 @@ int main(){
     }
 
     // initializing hash table to size of closest prime number to the right
-    hash_table_entry **hash_table = new hash_table_entry*[primeNumber];
+    auto **hash_table = new hash_table_entry*[primeNumber];
+
 
     // adding apps to hash table and bst
     for(int k = 0; k < howManyApps; k++){   // outer loop for number of apps
-        tree* newApp = new struct tree;
+        tree* newApp = new tree;
 
         getline(cin, input);
         newApp->record.category = input;
@@ -110,29 +138,30 @@ int main(){
         getline(cin, input);
         float price = stof(input);
         newApp->record.price = price;
-        newApp->left = NULL;
-        newApp->right = NULL;
+        newApp->left = nullptr;
+        newApp->right = nullptr;
+
 
         // inserting newApp into bst
         for(int l = 0; l < howManyCategories; l++){     // search app_categories array
             if(app_categories[l].category == newApp->record.category){      // checking for matching category
-                if(app_categories[l].root == NULL){     // checking if first node is NULL
+                if(app_categories[l].root == nullptr){     // checking if first node is NULL
                     app_categories[l].root = newApp;
                     break;
                 } else {                                // checking if newApp goes in left or right node until successful insert
                     bool openSpot = false;
+                    tree* temp = app_categories[l].root;          // temp node to traverse bst
                     while(!openSpot){
-                        tree* temp = app_categories->root;          // temp node to traverse bst
-                        if(temp->record.category > newApp->record.category){        // checking if node will go to left
-                            if(temp->left == NULL){                 // check if left node is currently NULL
+                        if(temp->record.category.compare(newApp->record.category) <= 0){        // checking if node will go to left
+                            if(temp->left == nullptr){                 // check if left node is currently NULL
                                 temp->left = newApp;
                                 break;       // exit while loop on successful insert
                             } else {                                // sets temp to left node and continues next traversal
                                 temp = temp->left;
                                 continue;
                             }
-                        } else if(temp->record.category < newApp->record.category){     // checking if node will go to right
-                            if(temp->right == NULL){                // check if right node is currently NULL
+                        } else if(temp->record.category.compare(newApp->record.category) > 0){     // checking if node will go to right
+                            if(temp->right == nullptr){                // check if right node is currently NULL
                                 temp->right = newApp;
                                 break;      // exit while loop on successful insert
                             } else {                                // sets temp to right node and continues next traversal
@@ -146,42 +175,45 @@ int main(){
             }
         }
 
+
         // inserting newApp into hash table
         int sum = 0;
-        for(int m = 0; m < newApp->record.category.length(); m++){  // go through string getting ascii value of each character
-            sum += newApp->record.category[m];
+        for(char m : newApp->record.app_name){  // go through string getting ascii value of each character
+            sum += m;
         }
         int hash = sum % primeNumber;                               // calculating the hash
-        if(hash_table[hash] == NULL){                               // checking if that entry is currently NULL
+        if(&hash_table[hash] == nullptr){                               // checking if that entry is currently NULL
+
             hash_table[hash]->app_name = newApp->record.app_name;
             hash_table[hash]->app_node = newApp;
-            hash_table[hash]->next = NULL;
+            hash_table[hash]->next = nullptr;
         } else {                                                    // if not NULL then make new entry
-            hash_table_entry* temp = new hash_table_entry;
+            auto* temp = new hash_table_entry;
             temp->app_node = newApp;                                // initialize entry
             temp->app_name = newApp->record.app_name;
             temp->next = hash_table[hash];                          // set next of entry to what is currently at hash
             hash_table[hash] = temp;                                // point hash to new entry
-        }
 
+        }
     }
 
+    // getting number of queries and turning into an int
     getline(cin, input);
     int numberOfQueries = stoi(input);
 
-    for(int h = 0; h < numberOfQueries; h++){
+    for(int h = 0; h < numberOfQueries; h++){               // loop through the queries one at a time
         getline(cin, input);
-        string whichQuery = input.substr(0,9);
-        if(whichQuery == "find app "){
-            string whichApp = input.substr(9,std::string::npos);
-            int sum = 0;
-            for(int n = 0; n < whichApp.length(); n++){
-                sum += whichApp[n];
+        string whichQuery = input.substr(0,9);      // substring the command
+        if(whichQuery == "find app "){                      // check if the substring is find app
+            string whichApp = input.substr(9);        // take substring of rest of input (the apps name)
+            int sum = 0;                                    // taking sum of ascii value for each char in app name
+            for(char n : whichApp){
+                sum += n;
             }
-            int hash = sum % primeNumber;
+            int hash = sum % primeNumber;                   // calculating hash
             bool found = false;
-            hash_table_entry* temp = hash_table[hash];
-            while(temp != NULL){
+            hash_table_entry* temp = hash_table[hash];      // initializing temp hash table entry to the current hash table hash
+            while(temp != nullptr){                            // if temp is not null then look for matching app name
                 if(temp->app_name == whichApp){
                     found = true;
                     break;
@@ -189,21 +221,48 @@ int main(){
                     temp = temp->next;
                 }
             }
-            if(found){
+
+            if(found){                                      // if found matching app name then go through the records and print its info
                 cout << "Found Application: " << whichApp << endl;
                 cout << "\tCategory: " << temp->app_node->record.category << endl;
                 cout << "\tApplication Name: " << temp->app_node->record.app_name << endl;
                 cout << "\tVersion: " << temp->app_node->record.version << endl;
-                cout << "\tSize: " << temp->app_node->record.size << endl;
+                cout << "\tSize: " << fixed << setprecision(1) << temp->app_node->record.size << endl;
                 cout << "\tUnits: " << temp->app_node->record.units << endl;
-                cout << "\tPrice: $" << temp->app_node->record.price << endl;
-            } else {
-                cout << "Application " << whichApp << " not found." << endl;
+                cout << "\tPrice: $" << fixed << setprecision(2) << temp->app_node->record.price << endl;
+            } else {                                        // app name was not found
+                cout << "Application " << whichApp.erase(whichApp.length() -1) << " not found." << endl;
+            }
+            if(h != numberOfQueries-1){
+                cout << endl;
+            }
+
+        }
+    }
+
+    // go through each index of hash table checking if NULL or not
+    for(int j = primeNumber-1; j >= 0; j--){
+        if(hash_table[j] == nullptr){
+            delete hash_table[j];
+        } else {                   // if not null then delete the nodes
+            hash_table_entry *temp = hash_table[j];
+            while(temp != nullptr){
+                hash_table_entry *temp2 = temp->next;
+                delete temp;
+                temp = temp2;
             }
         }
-
-        // write code for deallocating the memory (something getline with eof maybe)
     }
+    delete[] hash_table;        // completely delete the hash table array
+
+
+    // goes through each category of bst checking if the initial root is NULL or not
+    for(int i = howManyCategories-1; i >= 0; i--){
+        if(app_categories[i].root != nullptr){
+            deleteNode(app_categories[i].root);       // if root wasn't null send root to recursive delete func
+        }
+    }
+    delete[] app_categories;                // completely delete the app_categories array
 
     return 0;
 }
